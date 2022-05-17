cf4b4f8eeef2ac4bc208b98499f0e85d9f7b54d8
miccbatt <stage@zemconsulting.be>
2022-05-17 08:15:16 +0200

    users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration {
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->foreignIdFor(Language::class)->nullable();
-            $table->foreignId('role_id')->constrained()->nullable();
+            $table->foreignId('role_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
