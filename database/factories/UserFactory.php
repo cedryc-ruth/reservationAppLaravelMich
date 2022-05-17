859c5cc8c5c2ab92f64a682105809a654ab6f469
miccbatt <stage@zemconsulting.be>
2022-05-17 08:32:19 +0200

    users table

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => Hash::make('habaligani'),
             'remember_token' => Str::random(10),
-            'role_id'=>2,
+            'role_id'=>null,
             'language_id' => $this->faker->randomElement($languages),
         ];
 
